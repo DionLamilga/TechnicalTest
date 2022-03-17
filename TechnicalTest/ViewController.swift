c2ff352609b9671b021dcbe8ac6865663ce4f08f
TuanMudaDion <lamilgadion36@gmail.com>
2022-03-17 10:09:16 +0700

    Initial Commit

---
+++ TechnicalTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TechnicalTest
+//
+//  Created by Dion Lamilga on 17/03/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
